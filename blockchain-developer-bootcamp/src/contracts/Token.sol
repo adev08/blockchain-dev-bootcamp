114fd2e7bf59dae8e470ff46421e16dda65b4c35
Alex Mateo <vamateo85@gmail.com>
2021-04-02 07:33:19 -0400

    Create Smart Contract Unit Test (#4)
    * Project Setup
    
    * Smoke Test
    
    * Create Smart Contract Unit Test

--- blockchain-developer-bootcamp/src/contracts/Token.sol
+++ blockchain-developer-bootcamp/src/contracts/Token.sol
@@ -2,7 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
-    string public name = "My Name"; 
+    string public name = "ZodiacCoin Token";
+    string public symbol = "ZCoin";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+   constructor() public {
+       totalSupply = 1000000 * (10 ** decimals);
+   }
 }
 
 
