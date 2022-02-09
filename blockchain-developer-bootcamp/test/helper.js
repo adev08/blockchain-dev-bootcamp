bd2cd30147dac9853e3fea1be3fd642d3b1c2150
ALex Mateo <vamateo85@gmail.com>
2022-02-09 11:47:49 -0500

    remove duplicate files

--- blockchain-developer-bootcamp/test/helper.js
+++
@@ -1,9 +0,0 @@
-
-export const EVM_REVERT = 'VM Exception while processing transaction: revert'
-
-const Web3 = require('web3')
-export const tokens = (n) => {
-    return new Web3.utils.BN(
-        Web3.utils.toWei(n.toString(), 'ether')
-    )
-}  
